46c7301e2139c630cea91dc5d953e757c8fa7882
AnnaFragoso <anna.barbara.fragoso@hotmail.com>
2020-06-09 19:57:56 -0300

    Adicionando cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import 'express-async-errors';
 import routes from './routes';
+import cors from 'cors';
 import './database';
 
 class App {
     constructor() {
         this.server = express();
         this.server.use(express.json());
+        this.server.use(cors());
         this.routes();
     }
 
